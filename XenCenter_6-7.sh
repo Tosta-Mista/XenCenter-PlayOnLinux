b04b31fd082d387b02a06e63e41322493c90f44a
José Gonçalves <jose.goncalves@dlcproduction.ch>
2017-03-17 16:52:45 +0100

    Add last changes

--- XenCenter_6-7.sh
+++ XenCenter_6-7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Date : (2017-03-17 08:00)
 # Last revision : (2017-03-17 18:00)
-# Wine version used : 2.3
+# Wine version used : 1.9.6
 # Distribution used to test : Linux Mint 18.1 x64
 # Author : José Gonçalves
 # Licence : Retail
@@ -33,7 +33,7 @@ POL_Debug_Init
 POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$DOC_URL" "$AUTHOR" "$PREFIX"
  
 # Setting Wine Version
-WORKING_WINE_VERSION="2.3"
+WORKING_WINE_VERSION="1.9.6"
  
 # Setting prefix path
 POL_Wine_SelectPrefix "$PREFIX"
@@ -46,14 +46,6 @@ ARCHITECTURE="$APP_ANSWER"
 POL_System_SetArch "$ARCHITECTURE"
 POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
 
-# Seems to help with random crash issue
-Set_OS "win7"
-
-# Installing additional components
-POL_Call POL_install_dotnet40 
-POL_Call POL_Install_ie8
-POL_Call POL_Install_tahoma2
-
 # Choose between Downloading client or using local one
 POL_SetupWindow_InstallMethod "LOCAL"
  
@@ -63,32 +55,34 @@ POL_SetupWindow_VMS $GAME_VMS
 # Set Graphic Card information keys for wine
 POL_Wine_SetVideoDriver
 
+# Seems to help with random crash issue
+Set_OS "win7"
+
+# Installing additional components
+POL_Call POL_install_dotnet40 
+POL_Call POL_Install_ie8
+POL_Call POL_Install_tahoma2
+
 # Downloading client or choosing existing one
-mkdir -p "$WINEPREFIX/drive_c/$PROGRAMFILES/XenCenter/XenCenter6"
+mkdir -p "$WINEPREFIX/drive_c/$PROGRAMFILES/Citrix/XenCenter"
 
 if [ "$INSTALL_METHOD" = "DOWNLOAD" ]; then
         # Donwloading client
-        cd "$WINEPREFIX/drive_c/$PROGRAMFILES/XenCenter/XenCenter6"
-        if [ "$ARCHITECTURE" = "amd64" ]; then
-                POL_Download "http://downloadns.citrix.com.edgesuite.net/10341/XenServer-6.5.0-SP1-XenCenterSetup.exe"
-                FILENAME="XenServer-6.5.0-SP1-XenCenterSetup.exe"
-        else
-                POL_Download "http://downloadns.citrix.com.edgesuite.net/10341/XenServer-6.5.0-SP1-XenCenterSetup.exe"
-                FILENAME="XenServer-6.5.0-SP1-XenCenterSetup.exe"
-        fi
+        cd "$WINEPREFIX/drive_c/$PROGRAMFILES/Citrix/XenCenter"
+        POL_Download "http://downloadns.citrix.com.edgesuite.net/10341/XenServer-6.5.0-SP1-XenCenterSetup.exe"
+        FILENAME="XenServer-6.5.0-SP1-XenCenterSetup.exe"
+     
 elif [ "$INSTALL_METHOD" = "LOCAL" ]; then
         # Asking for client exe
         cd "$HOME"
         POL_SetupWindow_browse "$(eval_gettext 'Please select the setup file to run')" "$TITLE"
         SETUP_EXE="$APP_ANSWER"
-        cp "$SETUP_EXE" "$WINEPREFIX/drive_c/$PROGRAMFILES/XenCenter/XenCenter6/XenServer-6.5.0-SP1-XenCenterSetup.exe"
+        cp "$SETUP_EXE" "$WINEPREFIX/drive_c/$PROGRAMFILES/Citrix/XenCenter/XenServer-6.5.0-SP1-XenCenterSetup.exe"
+        POL_Wine "$WINEPREFIX/drive_c/$PROGRAMFILES/Citrix/XenCenter/XenServer-6.5.0-SP1-XenCenterSetup.exe"
 fi
  
 # Making shortcut
 POL_Shortcut "$FILENAME" "$TITLE" "xen_icon.png"
-
-# Begin installation
-cd "$WINEPREFIX/drive_c/$PROGRAMFILES/XenCenter/XenCenter6"
  
 POL_SetupWindow_Close
 exit 0
